7261d37a3e1d15cfbaf0ce6279f05f7d83679f80
ReaganKibet <reagankibetsang@gmail.com>
2024-11-24 08:05:38 +0300

    Adding org docs to ARC

---
+++ sql/organization_docs/Alignment-research-center-org-docs.sql
@@ -0,0 +1,42 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+    (
+        'https://forum.effectivealtruism.org/posts/JZqbNjtAqieivTG7Q/redwood-research-is-hiring-for-several-roles', /* url */
+        'Redwood Research is hiring for several roles', /* title */
+        '2021-11-29', /* publication_date */
+        NULL, /* modified_date */
+        'Jack R| Billz', /* author */
+        'Effective Altruism Forum', /* publisher */
+        'Alignment Research Center', /* affected_organizations */
+        NULL, /* affected_people */
+        'Hiring-related notice', /* document_scope */
+        NULL, /* cause_area */
+        'Jack R discusses Redwood Research’s hiring efforts, highlighting their applied AI alignment research and collaboration-focused Constellation space while outlining available roles and leadership involvement.' /* notes */
+    )
+    ,(
+        'https://forum.effectivealtruism.org/posts/YCAEbRFT8pha3JiXZ/hi-i-m-holden-karnofsky-ama-about-jobs-at-open-philanthropy', /* url */
+        'Apply to the ML for Alignment Bootcamp (MLAB) in Berkeley [Jan 3 - Jan 22]', /* title */
+        '2022-11-03', /* publication_date */
+        NULL, /* modified_date */
+        'Habryka| Buck', /* author */
+        'Effective Altruism Forum', /* publisher */
+        'Alignment Research Center| Redwood Research| Lightcone Infrastructure ', /* affected_organizations */
+        NULL, /* affected_people */
+        'General organization operations', /* document_scope */
+        NULL, /* cause_area */
+        'Redwood Research and Lightcone Infrastructure''s ML for Alignment Bootcamp provides a comprehensive hands-on learning experience in AI alignment, hosted at Berkeley''s Constellation alongside organizations like the Alignment Research Center..' /* notes */
+    )
+    ,(
+        'https://forum.effectivealtruism.org/posts/jmbP9rwXncfa32seH/after-one-year-of-applying-for-ea-jobs-it-is-really-really#comments', /* url */
+        'ARC is hiring alignment theory researchers', /* title */
+        '2021-12-14', /* publication_date */
+        NULL, /* modified_date */
+        'Paul_Christiano|  Mark Xu', /* author */
+        'Effective Altruism Forum', /* publisher */
+        'Alignment Research Center', /* affected_organizations */
+        NULL, /* affected_people */
+        'Hiring-related notice', /* document_scope */
+        'AI safety', /* cause_area */
+        'The article highlights the Alignment Research Center''s (ARC) focus on hiring researchers to develop strategies for aligning advanced machine learning systems with human interests, detailing qualifications, application processes, and its organizational goals.' /* notes */
+    )
+;
+
